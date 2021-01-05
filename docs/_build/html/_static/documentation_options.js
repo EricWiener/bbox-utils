c429f80d0b0b23aeb657b801da52e79b41d141b9
Eric Wiener <ericwiener3@gmail.com>
2021-01-04 21:43:57 -0500

    DOCS: switched docs theme to Read the Docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'unknown',
+    VERSION: '0.0.post0.dev3+g00a70d2.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
