92b12d62a3ee4f33ed899395541c4f81528a52e3
Eric Wiener <ericwiener3@gmail.com>
2021-01-05 09:22:51 -0500

    DOCS: updated documentation to reflect re-named files

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post0.dev3+g00a70d2.dirty',
+    VERSION: '0.0.post0.dev13+g978f407.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
