eccbd9013c00893b77e00c8d4f951db5665efd49
Eric Wiener <ericwiener3@gmail.com>
2021-01-05 09:05:44 -0500

    CHORE: excluded __init__ distribution not found from coverage

--- src/bbox_utils/__init__.py
+++ src/bbox_utils/__init__.py
@@ -5,7 +5,7 @@ try:
     # Change here if project is renamed and does not equal the package name
     dist_name = "bbox-utils"
     __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+except DistributionNotFound:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound
