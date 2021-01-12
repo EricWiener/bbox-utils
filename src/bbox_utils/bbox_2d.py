3bb6a69f65ccc7d3b83d67f6b82a13d049c5e891
Eric Wiener <ericwiener3@gmail.com>
2021-01-12 12:41:59 -0500

    FIX: fixed bugs with YOLO conversion. Updated tests

--- src/bbox_utils/bbox_2d.py
+++ src/bbox_utils/bbox_2d.py
@@ -21,14 +21,20 @@ class BoundingBox:
 
         Args:
             image_dimension (np.array): array with the image's dimensions
+            in form (rows, cols, depth)
 
         Returns:
             bool: whether the points are valid
         """
-        tl_valid = point_within_dimensions(self.tl, image_dimension)
-        tr_valid = point_within_dimensions(self.tr, image_dimension)
-        br_valid = point_within_dimensions(self.br, image_dimension)
-        bl_valid = point_within_dimensions(self.bl, image_dimension)
+        # Convert rows, cols, depth to x, y, z
+        xyz_dimension = np.copy(image_dimension)
+        xyz_dimension[0] = image_dimension[1]
+        xyz_dimension[1] = image_dimension[0]
+
+        tl_valid = point_within_dimensions(self.tl, xyz_dimension)
+        tr_valid = point_within_dimensions(self.tr, xyz_dimension)
+        br_valid = point_within_dimensions(self.br, xyz_dimension)
+        bl_valid = point_within_dimensions(self.bl, xyz_dimension)
 
         return tl_valid and tr_valid and br_valid and bl_valid
 
@@ -53,9 +59,9 @@ class BoundingBox:
             BoundingBox: a new bounding box instance
         """
         tl = top_left
-        tr = tl + np.array([0.0, width])
-        br = tl + np.array([height, width])
-        bl = tl + np.array([height, 0.0])
+        tr = tl + np.array([width, 0.0])
+        br = tl + np.array([width, height])
+        bl = tl + np.array([0.0, height])
         points = np.array([tl, tr, br, bl])
         box = BoundingBox(points, ordered=True)
         return box
@@ -102,25 +108,36 @@ class BoundingBox:
             bottom_right[0] > 0 and bottom_right[1] > 0
         ), "bottom_right point has negative value {}".format(bottom_right)
 
-        tl = top_left
-        br = bottom_right
-        height = br[0] - tl[0]
-        width = br[1] - tl[1]
-        tr = top_left + np.array([0.0, width])
-        bl = top_left + np.array([height, 0.0])
-        points = np.array([tl, tr, br, bl])
+        t_x, t_y = top_left
+        b_x, b_y = bottom_right
+        height = b_y - t_y
+        width = b_x - t_x
+        tr = top_left + np.array([width, 0.0])
+        bl = top_left + np.array([0.0, height])
+        points = np.array([top_left, tr, bottom_right, bl])
         box = BoundingBox(points, ordered=True)
         return box
 
     def to_yolo(self, image_dimension):
+        """Generates a YOLO formatted np.array with center_x, center_y, width, height
+
+        Args:
+            image_dimension (np.array): array with image dimensions of form
+            (rows, cols) or (rows, cols, depth)
+
+        Returns:
+            np.array: array with YOLO formatted bounding box
+        """
+
         # Make sure all points are within dimensions
         assert self.validate_points(
             image_dimension
-        ), "Some points of the bounding box lie outside of the image dimensions"
+        ), "Some points of the bounding box lie outside of image dimensions"
 
         cx, cy = self.center
 
         # Get normalized dimensions
+        # (make sure to use rows for height and cols for width)
         img_h, img_w = image_dimension[:2]
 
         cx = float(cx) / img_w
@@ -136,9 +153,14 @@ class BoundingBox:
 
     @property
     def center(self):
-        cx = self.tl[0] + self.width // 2
-        cy = self.tl[1] + self.height // 2
-        return np.array([cx, cy]).astype(int)
+        """Returns the center point of the bounding box as (x, y)
+
+        Returns:
+            np.array: center of bounding box in (x, y) form. NOT (row, column) form.
+        """
+        x = self.tl[0] + self.width // 2
+        y = self.tl[1] + self.height // 2
+        return np.array([x, y]).astype(int)
 
     @property
     def width(self):
--- tests/test_bbox_2d.py
+++ tests/test_bbox_2d.py
@@ -56,8 +56,8 @@ def test_box_from_xyxy():
     bbox = BoundingBox.from_xyxy(xy1, xy2)
     xy, w, h = bbox.to_xywh()
     assert np.array_equal(xy, xy1)
-    assert w == 70
-    assert h == 40
+    assert w == 40
+    assert h == 70
 
 
 def test_box_from_xyxy_swapped_order():
@@ -68,8 +68,8 @@ def test_box_from_xyxy_swapped_order():
     bbox = BoundingBox.from_xyxy(xy2, xy1)
     xy, w, h = bbox.to_xywh()
     assert np.array_equal(xy, xy1)
-    assert w == 70
-    assert h == 40
+    assert w == 40
+    assert h == 70
 
 
 def test_box_from_xyxy_horizontal_line():
@@ -112,32 +112,26 @@ def test_box_from_xywh():
     h = xywh[3]
     bbox = BoundingBox.from_xywh(xy, w, h)
     xyxy = bbox.to_xyxy()
-    assert np.array_equal(xyxy, np.array([[2369, 2975], [2369 + 78, 2975 + 74]]))
+    assert np.array_equal(xyxy, np.array([[2369, 2975], [2369 + w, 2975 + h]]))
 
 
 def test_bounding_to_yolo():
     """Test to see if bounding box can be converted to YOLO"""
-    image_dim = (3648, 5472)
+    image_dim = np.array([5472, 3648])
     xywh = np.array([2369, 2975, 74, 78])
     xy = xywh[:2]
     w = xywh[2]
     h = xywh[3]
     bbox = BoundingBox.from_xywh(xy, w, h)
-    correct = np.array(
-        [
-            0.43969298245614036,
-            0.8262061403508771,
-            0.013523391812865496,
-            0.02138157894736842,
-        ]
-    )
-    assert np.array_equal(bbox.to_yolo(image_dim), correct)
+    correct = np.array([0.65953947, 0.55080409, 0.02028509, 0.01425439])
+    yolo = bbox.to_yolo(image_dim)
+    assert np.allclose(yolo, correct)
 
 
 def test_bounding_to_yolo_invalid_points():
     """Test assertion is thrown when creating YOLO
     coordinates with out of bound point"""
-    image_dim = (3648, 5472)
+    image_dim = np.array([5472, 3648])
     xy1 = np.array([3640, 78])
     xy2 = np.array([3650, 100])
     bbox = BoundingBox.from_xyxy(xy1, xy2)
@@ -146,10 +140,27 @@ def test_bounding_to_yolo_invalid_points():
         _ = bbox.to_yolo(image_dim)
 
 
+def test_bounding_to_yolo_small_box():
+    """Make sure YOLO conversion works correctly with a small bounding box"""
+    image_dim = np.array([3648, 5472])
+    xy1 = np.array([4360, 971])
+    xy2 = np.array([4397, 998])
+    bbox = BoundingBox.from_xyxy(xy1, xy2)
+    yolo = bbox.to_yolo(image_dim)
+    assert np.allclose(yolo, np.array([0.8000731, 0.26973684, 0.0067617, 0.00740132]))
+
+    image_dim = np.array([3456, 4608])
+    xy1 = np.array([3155, 3432])
+    xy2 = np.array([3234, 3455])
+    bbox = BoundingBox.from_xyxy(xy1, xy2)
+    yolo = bbox.to_yolo(image_dim)
+    assert np.allclose(yolo, np.array([0.69314236, 0.99623843, 0.0171441, 0.00665509]))
+
+
 def test_bounding_box_center():
-    xywh = np.array([2369, 2975, 74, 78])
+    xywh = np.array([2369, 2975, 74, 201])
     xy = xywh[:2]
     w = xywh[2]
     h = xywh[3]
     bbox = BoundingBox.from_xywh(xy, w, h)
-    assert np.array_equal(bbox.center, np.array([2406, 3014]))
+    assert np.array_equal(bbox.center, np.array([2406, 3075]))
