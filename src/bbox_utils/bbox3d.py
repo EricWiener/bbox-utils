6740b8e34cef6206573015b7e66df86b5e9e9232
Eric Wiener <ericwiener3@gmail.com>
2021-01-16 16:24:43 -0500

    FEAT: added bbox3d functions from PyPI bbox module
    The original source of the code is:
    https://github.com/varunagrawal/bbox/blob/master/bbox/bbox3d.py
    
    I fixed a failing test, made the linting pass, and removed un-necessary
    getters and setters.

---
+++ src/bbox_utils/bbox3d.py
@@ -0,0 +1,318 @@
+"""3D bounding box module. This file is modified from
+https://github.com/varunagrawal/bbox/blob/master/bbox/bbox3d.py."""
+
+# pylint: disable=invalid-name,missing-docstring
+
+from copy import deepcopy
+
+import numpy as np
+from pyquaternion import Quaternion
+
+
+class BBox3D:
+    """
+    Class for 3D Bounding Boxes (3-orthotope).
+    It takes either the center of the 3D bounding box or the \
+        back-bottom-left corner, the width, height and length \
+        of the box, and quaternion values to indicate the rotation.
+    Args:
+        x (:py:class:`float`): X axis coordinate of 3D bounding box. \
+            Can be either center of bounding box or back-bottom-left corner.
+        y (:py:class:`float`): Y axis coordinate of 3D bounding box. \
+            Can be either center of bounding box or back-bottom-left corner.
+        z (:py:class:`float`): Z axis coordinate of 3D bounding box. \
+            Can be either center of bounding box or back-bottom-left corner.
+        length (:py:class:`float`, optional): The length of the box (default is 1).
+        width (:py:class:`float`, optional): The width of the box (default is 1).
+        height (:py:class:`float`, optional): The height of the box (default is 1).
+        rw (:py:class:`float`, optional): The real part of the rotation quaternion \
+            (default is 1).
+        rx (:py:class:`int`, optional): The first element of the quaternion vector \
+            (default is 0).
+        ry (:py:class:`int`, optional): The second element of the quaternion vector \
+            (default is 0).
+        rz (:py:class:`int`, optional): The third element of the quaternion vector \
+            (default is 0).
+        euler_angles (:py:class:`list` or :py:class:`ndarray` of float, optional): \
+            Sequence of euler angles in `[x, y, z]` rotation order \
+            (the default is None).
+        is_center (`bool`, optional): Flag to indicate if the provided coordinate \
+            is the center of the box (the default is True).
+    """
+
+    def __init__(
+        self,
+        x,
+        y,
+        z,
+        length=1,
+        width=1,
+        height=1,
+        rw=1,
+        rx=0,
+        ry=0,
+        rz=0,
+        q=None,
+        euler_angles=None,
+        is_center=True,
+    ):
+        if is_center:
+            self._cx, self._cy, self._cz = x, y, z
+            self._c = np.array([x, y, z])
+        else:
+            self._cx = x + length / 2
+            self._cy = y + width / 2
+            self._cz = z + height / 2
+            self._c = np.array([self._cx, self._cy, self._cz])
+
+        self._w, self._h, self._l = width, height, length
+
+        if euler_angles:
+            # we need to apply y, z and x rotations in order
+            # http://www.euclideanspace.com/maths/geometry/rotations/euler/index.htm
+            self._q = (
+                Quaternion(axis=[0, 1, 0], angle=euler_angles[1])
+                * Quaternion(axis=[0, 0, 1], angle=euler_angles[2])
+                * Quaternion(axis=[1, 0, 0], angle=euler_angles[0])
+            )
+
+        elif q is not None:
+            self._q = Quaternion(q)
+        else:
+            self._q = Quaternion(rw, rx, ry, rz)
+
+    @property
+    def center(self):
+        """
+        Attribute to access center coordinates of box in (x, y, z) format.
+        Can be set to :py:class:`list` or :py:class:`ndarray` of float.
+        Returns:
+            :py:class:`ndarray` of float: 3-dimensional vector representing \
+                (x, y, z) coordinates of the box.
+        Raises:
+            ValueError: If `c` is not a vector/list of length 3.
+        """
+        return self._c
+
+    @center.setter
+    def center(self, c):
+        if len(c) != 3:
+            raise ValueError("Center coordinates should be a vector of size 3")
+        self._c = c
+
+    def __valid_scalar(self, x):
+        if not np.isscalar(x):
+            raise ValueError("Value should be a scalar")
+        else:  # x is a scalar so we check for numeric type
+            if not isinstance(x, (np.float, np.int)):
+                raise TypeError("Value needs to be either a float or an int")
+        return x
+
+    @property
+    def cx(self):
+        """
+        :py:class:`float`: X coordinate of center.
+        """
+        return self._cx
+
+    @cx.setter
+    def cx(self, x):
+        self._cx = self.__valid_scalar(x)
+
+    @property
+    def cy(self):
+        """
+        :py:class:`float`: Y coordinate of center.
+        """
+        return self._cy
+
+    @cy.setter
+    def cy(self, x):
+        self._cy = self.__valid_scalar(x)
+
+    @property
+    def cz(self):
+        """
+        :py:class:`float`: Z coordinate of center.
+        """
+        return self._cz
+
+    @cz.setter
+    def cz(self, x):
+        self._cz = self.__valid_scalar(x)
+
+    @property
+    def q(self):
+        """
+        Syntactic sugar for the rotation quaternion of the box.
+        Returns
+            :py:class:`ndarray` of float: Quaternion values in (w, x, y, z) form.
+        """
+        return np.hstack((self._q.real, self._q.imaginary))
+
+    @q.setter
+    def q(self, q):
+        if not isinstance(q, (list, tuple, np.ndarray, Quaternion)):
+            raise TypeError("Value shoud be either list, numpy array or Quaterion")
+        if isinstance(q, (list, tuple, np.ndarray)) and len(q) != 4:
+            raise ValueError("Quaternion input should be a vector of size 4")
+
+        self._q = Quaternion(q)
+
+    @property
+    def quaternion(self):
+        """
+        The rotation quaternion.
+        Returns:
+            :py:class:`ndarray` of float: Quaternion values in (w, x, y, z) form.
+        """
+        return self.q
+
+    @quaternion.setter
+    def quaternion(self, q):
+        self.q = q
+
+    @property
+    def length(self):
+        """
+        :py:class:`float`: Length of the box.
+        """
+        return self._l
+
+    @length.setter
+    def length(self, x):
+        self._l = self.__valid_scalar(x)
+
+    @property
+    def width(self):
+        """
+        :py:class:`float`: The width of the box.
+        """
+        return self._w
+
+    @width.setter
+    def width(self, x):
+        self._w = self.__valid_scalar(x)
+
+    @property
+    def height(self):
+        """
+        :py:class:`float`: The height of the box.
+        """
+        return self._h
+
+    @height.setter
+    def height(self, x):
+        self._h = self.__valid_scalar(x)
+
+    def __transform(self, x):
+        """
+        Rotate and translate the point to world coordinates.
+        """
+        y = self._c + self._q.rotate(x)
+        return y
+
+    @property
+    def p1(self):
+        """
+        :py:class:`float`: Back-left-bottom point.
+        """
+        p = np.array([-self._l / 2, -self._w / 2, -self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p2(self):
+        """
+        :py:class:`float`: Front-left-bottom point.
+        """
+        p = np.array([self._l / 2, -self._w / 2, -self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p3(self):
+        """
+        :py:class:`float`: Front-right-bottom point.
+        """
+        p = np.array([self._l / 2, self._w / 2, -self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p4(self):
+        """
+        :py:class:`float`: Back-right-bottom point.
+        """
+        p = np.array([-self._l / 2, self._w / 2, -self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p5(self):
+        """
+        :py:class:`float`: Back-left-top point.
+        """
+        p = np.array([-self._l / 2, -self._w / 2, self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p6(self):
+        """
+        :py:class:`float`: Front-left-top point.
+        """
+        p = np.array([self._l / 2, -self._w / 2, self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p7(self):
+        """
+        :py:class:`float`: Front-right-top point.
+        """
+        p = np.array([self._l / 2, self._w / 2, self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p8(self):
+        """
+        :py:class:`float`: Back-right-top point.
+        """
+        p = np.array([-self._l / 2, self._w / 2, self._h / 2])
+        p = self.__transform(p)
+        return p
+
+    @property
+    def p(self):
+        """
+        Attribute to access ndarray of all corners of box in order.
+        Returns:
+            :py:class:`ndarray` of float: All corners of the bounding box in order.
+        """
+        x = np.vstack(
+            [self.p1, self.p2, self.p3, self.p4, self.p5, self.p6, self.p7, self.p8]
+        )
+        return x
+
+    def __repr__(self):
+        template = (
+            "BBox3D(x={cx}, y={cy}, z={cz}), length={l}, width={w}, height={h}, "
+            "q=({rw}, {rx}, {ry}, {rz}))"
+        )
+        return template.format(
+            cx=self._cx,
+            cy=self._cy,
+            cz=self._cz,
+            l=self._l,
+            w=self._w,
+            h=self._h,
+            rw=self._q.real,
+            rx=self._q.imaginary[0],
+            ry=self._q.imaginary[1],
+            rz=self._q.imaginary[2],
+        )
+
+    def copy(self):
+        return deepcopy(self)
---
+++ tests/test_bbox_3d.py
@@ -0,0 +1,324 @@
+import numpy as np
+import pytest
+from PIL import Image, ImageDraw
+
+from bbox_utils import BBox3D
+
+
+class TestBBox3d:
+    @classmethod
+    def setup_class(cls):
+        # sample cuboid
+        cuboid = {
+            "center": {
+                "x": -49.19743041908411,
+                "y": 12.38666074615689,
+                "z": 0.782056864653507,
+            },
+            "dimensions": {
+                "length": 5.340892485711914,
+                "width": 2.457703972075464,
+                "height": 1.9422248281533563,
+            },
+            "rotation": {
+                "w": 0.9997472337219893,
+                "x": 0.0,
+                "y": 0.0,
+                "z": 0.022482630300529462,
+            },
+        }
+        center = cuboid["center"]
+        dim = cuboid["dimensions"]
+        rotation = cuboid["rotation"]
+
+        cls.box = BBox3D(
+            center["x"],
+            center["y"],
+            center["z"],
+            length=dim["length"],
+            width=dim["width"],
+            height=dim["height"],
+            rw=rotation["w"],
+            rx=rotation["x"],
+            ry=rotation["y"],
+            rz=rotation["z"],
+        )
+        cls.cuboid = cuboid
+
+    def test_points(self):
+        points = np.array(
+            [
+                [-51.80993533, 11.03900409, -0.18905555],
+                [-46.47444215, 11.27909801, -0.18905555],
+                [-46.58492551, 13.7343174, -0.18905555],
+                [-51.92041869, 13.49422348, -0.18905555],
+                [-51.80993533, 11.03900409, 1.75316928],
+                [-46.47444215, 11.27909801, 1.75316928],
+                [-46.58492551, 13.7343174, 1.75316928],
+                [-51.92041869, 13.49422348, 1.75316928],
+            ]
+        )
+
+        assert np.allclose(self.box.p1, points[0, :])
+        assert np.allclose(self.box.p2, points[1, :])
+        assert np.allclose(self.box.p3, points[2, :])
+        assert np.allclose(self.box.p4, points[3, :])
+        assert np.allclose(self.box.p5, points[4, :])
+        assert np.allclose(self.box.p6, points[5, :])
+        assert np.allclose(self.box.p7, points[6, :])
+        assert np.allclose(self.box.p8, points[7, :])
+
+        assert np.allclose(self.box.p, points)
+
+    def test_center(self):
+        center = np.array([-49.19743041908411, 12.38666074615689, 0.782056864653507])
+        assert np.array_equal(self.box.center, center)
+
+    def test_center_points(self):
+        assert self.box.cx == self.cuboid["center"]["x"]
+        assert self.box.cy == self.cuboid["center"]["y"]
+        assert self.box.cz == self.cuboid["center"]["z"]
+
+    def test_init_center(self):
+        box = BBox3D(*[self.box.cx, self.box.cy, self.box.cz], is_center=True)
+        assert np.array_equal(box.center, self.box.center)
+
+    def test_init_non_center(self):
+        cx, cy, cz = self.box.cx, self.box.cy, self.box.cz
+        box = BBox3D(
+            cx - self.box.length / 2,
+            cy - self.box.width / 2,
+            cz - self.box.height / 2,
+            self.box.length,
+            self.box.width,
+            self.box.height,
+            is_center=False,
+        )
+        assert np.array_equal(box.center, self.box.center)
+
+    def test_center_setter(self):
+        box = self.box.copy()
+        box.center = np.asarray([0, 1, 2])
+        assert np.array_equal(box.center, np.asarray([0, 1, 2]))
+
+        with pytest.raises(ValueError):
+            box.center = np.asarray([0, 1])
+            box.center = np.asarray([0, 1, 2, 3])
+
+    def test_dimensions(self):
+        assert self.box.length == self.cuboid["dimensions"]["length"]
+        assert self.box.width == self.cuboid["dimensions"]["width"]
+        assert self.box.height == self.cuboid["dimensions"]["height"]
+
+    def test_quaternion(self):
+        q = np.array([0.9997472337219893, 0.0, 0.0, 0.022482630300529462])
+        assert np.array_equal(self.box.q, q)
+        # alternative attribute for the quaternion
+        assert np.array_equal(self.box.quaternion, q)
+
+        box = BBox3D(self.box.cx, self.box.cy, self.box.cz, q=self.box.q)
+        assert np.array_equal(self.box.q, box.q)
+
+    def test_euler_angles(self):
+        box = BBox3D(
+            3.163,
+            z=2.468,
+            y=34.677,
+            height=1.529,
+            width=1.587,
+            length=3.948,
+            euler_angles=[0, 0, -1.59],
+        )
+        q = np.array([0.7002847660410397, 0.0, 0.0, -0.713863604935036])
+        assert np.allclose(box.q, q)
+
+    def test_projection(self):
+        K = np.array(
+            [
+                [1406.3359, 0.0, 966.366034, 0.0],
+                [0.0, 1408.94297, 607.479746, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+            ]
+        )
+
+        R = np.array(
+            [
+                [0.50478576, 0.86323317, -0.00445338],
+                [-0.00422247, -0.00268975, -0.99998747],
+                [-0.86323433, 0.50479824, 0.00228723],
+            ]
+        )
+
+        t = np.array([[-0.75116634], [1.35776453], [0.87137971]])
+
+        u = np.empty((8, 2))
+        u[0] = self.project(self.box.p1, K, R, t)
+        u[1] = self.project(self.box.p2, K, R, t)
+        u[2] = self.project(self.box.p3, K, R, t)
+        u[3] = self.project(self.box.p4, K, R, t)
+        u[4] = self.project(self.box.p5, K, R, t)
+        u[5] = self.project(self.box.p6, K, R, t)
+        u[6] = self.project(self.box.p7, K, R, t)
+        u[7] = self.project(self.box.p8, K, R, t)
+
+        image_points = np.array(
+            [
+                [488.84269983, 655.2790429],
+                [530.3490365, 659.17142666],
+                [602.90920984, 657.55445187],
+                [556.26022377, 653.89914093],
+                [488.64644505, 601.79933826],
+                [530.12998103, 600.55433066],
+                [602.68953074, 600.56675254],
+                [556.06325411, 601.7790501],
+            ]
+        )
+
+        for i in range(8):
+            assert np.allclose(u[i], image_points[i])
+
+    def test_setters(self):
+        x, y, z, height, width, length = np.random.rand(6)
+        box = self.box.copy()
+        box.cx = x
+        box.cy = y
+        box.cz = z
+        box.height = height
+        box.width = width
+        box.length = length
+        assert box.cx == x
+        assert box.cy == y
+        assert box.cz == z
+        assert box.height == height
+        assert box.width == width
+        assert box.length == length
+
+        q = np.random.rand(4)
+        box.q = q
+        assert np.array_equal(box.q, q)
+        q = np.random.rand(4)
+        box.quaternion = q
+        assert np.array_equal(box.quaternion, q)
+
+        center = np.random.rand(3)
+        box.center = center
+        assert np.array_equal(box.center, center)
+
+    def test_bad_setters(self):
+        inputs = [[1, 2], np.zeros((3)), np.zeros((3, 1)), np.zeros((1, 3)), "center"]
+        for x in inputs:
+            with pytest.raises((ValueError, TypeError)):
+                self.box.cx = x
+            with pytest.raises((ValueError, TypeError)):
+                self.box.cy = x
+            with pytest.raises((ValueError, TypeError)):
+                self.box.cz = x
+            with pytest.raises((ValueError, TypeError)):
+                self.box.height = x
+            with pytest.raises((ValueError, TypeError)):
+                self.box.width = x
+            with pytest.raises((ValueError, TypeError)):
+                self.box.length = x
+
+        quaternion_inputs = [1, [1], [1, 2, 3], [1, 2, 3, 4, 5], "quaternion"]
+        for q in quaternion_inputs:
+            with pytest.raises((ValueError, TypeError)):
+                self.box.q = q
+
+    def test_render(self):
+        K = np.array(
+            [
+                [1406.3359, 0.0, 966.366034, 0.0],
+                [0.0, 1408.94297, 607.479746, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+            ]
+        )
+
+        R = np.array(
+            [
+                [0.50478576, 0.86323317, -0.00445338],
+                [-0.00422247, -0.00268975, -0.99998747],
+                [-0.86323433, 0.50479824, 0.00228723],
+            ]
+        )
+
+        t = np.array([[-0.75116634], [1.35776453], [0.87137971]])
+
+        u = np.empty((8, 2))
+        u[0] = self.project(self.box.p1, K, R, t)
+        u[1] = self.project(self.box.p2, K, R, t)
+        u[2] = self.project(self.box.p3, K, R, t)
+        u[3] = self.project(self.box.p4, K, R, t)
+        u[4] = self.project(self.box.p5, K, R, t)
+        u[5] = self.project(self.box.p6, K, R, t)
+        u[6] = self.project(self.box.p7, K, R, t)
+        u[7] = self.project(self.box.p8, K, R, t)
+
+        img = Image.new(mode="RGB", size=(512, 512))
+
+        dist_coeff = [-0.17120984449230167, 0.1256910189977147, -0.029726711792577232]
+
+        for i in range(u.shape[0]):
+            u[i] = self.distortion_correction(u[i], dist_coeff, img.size)
+
+        img = self.draw_cuboid(img, u)
+        # img.show()
+
+    @staticmethod
+    def project(p, K, R, t):
+        p = np.hstack((p, 1))
+        E = np.eye(4)
+        E[0:3, 0:3] = R
+        E[0:3, 3:4] = t
+
+        u = K @ E @ p
+        # print("projection", u)
+        return u[0:2] / u[2]
+
+    @staticmethod
+    def distortion_correction(u, dist_coeff, img_size):
+        w, h = img_size
+        # normalize the image coords
+        x = 2 * u[0] / w - 1
+        y = 2 * u[1] / h - 1
+        r = np.linalg.norm(np.array(x, y))
+
+        r2 = r ** 2
+
+        distortion = 0
+        for i in range(len(dist_coeff)):
+            distortion += (r2 ** (i + 1)) * dist_coeff[i]
+
+        # correct for distortion
+        v = u + (u - np.array([w, h]) / 2) * distortion
+        return v
+
+    @staticmethod
+    def draw_cuboid(img, p, color=None):
+        draw = ImageDraw.Draw(img)
+        color = color or tuple(np.random.choice(range(256), size=3))
+
+        draw.line([p[0][0], p[0][1], p[1][0], p[1][1]], fill=color, width=2)
+        draw.line([p[1][0], p[1][1], p[5][0], p[5][1]], fill=color, width=2)
+        draw.line([p[5][0], p[5][1], p[4][0], p[4][1]], fill=color, width=2)
+        draw.line([p[4][0], p[4][1], p[0][0], p[0][1]], fill=color, width=2)
+
+        draw.line([p[3][0], p[3][1], p[2][0], p[2][1]], fill=color, width=2)
+        draw.line([p[2][0], p[2][1], p[6][0], p[6][1]], fill=color, width=2)
+        draw.line([p[6][0], p[6][1], p[7][0], p[7][1]], fill=color, width=2)
+        draw.line([p[7][0], p[7][1], p[3][0], p[3][1]], fill=color, width=2)
+
+        draw.line([p[0][0], p[0][1], p[3][0], p[3][1]], fill=color, width=2)
+        draw.line([p[1][0], p[1][1], p[2][0], p[2][1]], fill=color, width=2)
+        draw.line([p[5][0], p[5][1], p[6][0], p[6][1]], fill=color, width=2)
+        draw.line([p[4][0], p[4][1], p[7][0], p[7][1]], fill=color, width=2)
+        return img
+
+    def test_repr(self):
+        representation = (
+            "BBox3D(x=-49.19743041908411, y=12.38666074615689, z=0.782056864653507), "
+            "length=5.340892485711914, width=2.457703972075464, "
+            "height=1.9422248281533563, "
+            "q=(0.9997472337219893, 0.0, 0.0, 0.022482630300529462))"
+        )
+        assert repr(self.box) == representation
