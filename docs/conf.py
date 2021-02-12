5546ee254b0a721ed877ba6b0d793af429884878
Eric Wiener <ericwiener3@gmail.com>
2021-02-12 16:08:58 -0500

    FIX: fixed issue with using wrong directory for docs building

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../src/bbox")
+module_dir = os.path.join(__location__, "../src/bbox_utils")
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
